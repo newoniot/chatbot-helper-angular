4e089617c851dfc801c929a9f16eadd4f28d9d5b
newoniot <newoniot@gmail.com>
2020-09-01 03:00:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-aqsyfk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-aqsyfk)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, VERSION, OnInit } from '@angular/core';
+import liff from 'liff-v2-sdk';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  implements OnInit {
+
+  username: ''
+  password: ''
+
+  ngOnInit() { console.log(`OnInit`); 
+  //alert('before create');
+    liff.init({ liffId: "https://liff.line.me/1654367482-5LkZWvre" }, () => {
+      if (liff.isLoggedIn()) {
+        // ขั้นตอน 4.4 รออยู่
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err.code, err.message));
+  }
+  
+login() {
+    alert('login');
+    liff.getProfile().then(profile => {
+      // const userProfile = profile.userId;
+      const displayName = profile.displayName;
+      // const statusMessage = profile.statusMessage;
+      // const pictureUrl = profile.pictureUrl;
+      // const email = liff.getDecodedIDToken().email;
+      alert('displayName : '+displayName);
+    }).catch(
+      err => console.error(err)
+    );
+    // this.$store.dispatch('addTodo', {
+    //   task: this.task,
+    //   newId: this.newId
+    // })
+    // this.task = ''
+  }
+
+}
